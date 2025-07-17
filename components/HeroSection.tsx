5f77f08cb5875e7bc1f39952641da7a58f910b55
umarapiantae <umarapiantae@gmail.com>
2025-07-17 22:30:31 +0800

    Update hero section, ganti background & font, favicon

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
       className="bg-cover bg-center py-[70px] px-5 md:px-[75px] text-white"
       style={{
         backgroundImage:
-          "url('https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgRtN431P2zjoHQDSo-lxXDK1EVJQ8xfQ4PbCLo7cTB00WeRlSSURX-Qdp3-rsxfNwnC9vCxWq-5lxeNZmT9hRYyEJkZ0nF7mGqZf_AHmmWbYOhzb8uDFUip1xQKGUgtcTPDpxshIy9N9Ao3GwEjWd-fGQoyaVblR3IrRPWkvLqCGbjNUZPCkRpel9P2kg')",
+          "url('/images/hero-bg.webp')"
       }}
     >
       <img
@@ -12,11 +12,11 @@ const HeroSection = () => {
         src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEhh-m3RYJG23xU7skEP18A7nEJFIg7wJ80Dx5k53RiRbEChyphenhyphenb24RZeEPk9Um469ip5jJxy4kS4Fe07QgdgRm0jKRo8tMUNf-0ipDqMjdEi1tImnyNdsvERImGZC0Dlrr-HGmQLibhPet4JHIbPqBJQo0tIYeX6VdqaX2g6FkGSSxqs-S-WkFND3mvciWhY/s3264/logo%20sidi.png"
         alt="Logo SiDi"
       />
-      <div className="text-[#f2cc30] text-[17px] font-light mb-2">Selamat datang di</div>
-      <h1 className="text-[50px] md:text-[80px] font-light w-full md:w-[500px] mb-6">Sistem Digitalisasi</h1>
-      <p className="text-lg font-light w-full md:w-[500px] mb-10 text-[#f0eeee]">
+      <div className="text-[#f2cc30] text-[17px] font-light mb-0 font-garet">Selamat datang di</div>
+      <h1 className="text-[50px] md:text-[70px] font-light w-full md:w-[500px] mb-5 text-black font-garet">Sistem Digitalisasi</h1>
+      <p className="text-lg font-garet font-light w-full md:w-[500px] mb-5 text-[#f0eeee]">
         Merancang dan mengembangkan struktur dasar dokumen, aplikasi, dan desain dalam format digital. Lihat produk digital kami{' '}
-        <a href="#" className="text-blue-700 hover:underline">disini!</a>
+        <a href="#" className="text-blue-700 hover:underline">disini</a>, atau klik tombol "Cari Produk" di bawah ini:
       </p>
       <button className="w-[160px] h-[50px] rounded-lg bg-[#e7ecee] text-[#1449a2] font-bold border border-[#5789ad] hover:bg-[#d6dde0] hover:text-[#0e356e] active:bg-[#5789ad] active:text-white transition">
         Cari Produk
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        garet: ['Garet', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
